c39e3ce410b9b28e770e801f41e7f338736fcc1c
Leopold Lemmermann <leopold@lemmermann.com>
2022-11-17 22:16:59 +0100

    Added the newest version and some older versions source code.

---
+++ v2/Anagrams/AnagramsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnagramsApp.swift
+//  Anagrams
+//
+//  Created by Leopold Lemmermann on 08.02.22.
+//
+
+import SwiftUI
+
+@main
+struct AnagramsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
